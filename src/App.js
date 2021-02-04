6d3db5887fcad428f0ffef9e56f030d2dcefc0e9
lu15v <lu15bytes@gmail.com>
2021-02-04 00:50:06 -0600

    FEATURE: semantic ui setup with: login, register, home components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/pages/Home'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import MenuBar from './components/MenuBar';
+import {Container} from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
 
 function App() {
   return (
-    <div>
-      <p>hola mundo</p>
-    </div>
+   <Router>
+     <Container>
+        <MenuBar/>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/register' component={Register}></Route>
+     </Container>
+   </Router>
   );
 }
 
