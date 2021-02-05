34559889320b41e886ffb33795e77071fb9ad934
lu15v <lu15bytes@gmail.com>
2021-02-05 01:00:08 -0600

    FEATURE: postCard component created, it renders the cards from the back end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './components/pages/Home'
-import Login from './components/pages/Login'
-import Register from './components/pages/Register'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import MenuBar from './components/MenuBar';
 import {Container} from 'semantic-ui-react';
 
---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {useQuery} from '@apollo/react-hooks';
+import { Grid, Image } from 'semantic-ui-react'
+import gql from 'graphql-tag';
+
+import PostCard from '../components/PostCard';
+
+const  Home = () =>{
+    //const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS);
+    const {loading, data} = useQuery(FETCH_POSTS);
+
+    return (
+        <Grid columns={3} divided>
+            <Grid.Row className="page-title">
+                <h1>Recent Posts</h1>
+            </Grid.Row>
+            <Grid.Row>
+            {loading ? (
+                <p>loading...</p>
+            ): (
+                data.getPosts  && data.getPosts.map(post =>(
+                <Grid.Column key={post.id} style={{marginBotton: 20}}>
+                    <PostCard post={post}/>
+                </Grid.Column>
+                ))
+            )}
+            </Grid.Row>
+        </Grid>
+    )
+}
+ 
+const FETCH_POSTS = gql`
+{
+    getPosts{
+        id
+        body
+        createdAt
+        username
+        likeCount
+        likes{
+            username
+        }
+        commentCount
+        comments{
+            id
+            username
+            createdAt
+            body
+        }
+        
+    }
+}
+`;
+export default Home;
