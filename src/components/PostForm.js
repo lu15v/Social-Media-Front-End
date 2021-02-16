934abdc80aed9db7787ff9112b4a2fadd8a6199e
lu15v <lu15bytes@gmail.com>
2021-02-15 17:40:53 -0800

    FEATURE: create post implemented and refreshing after mutation

---
+++ src/components/PostForm.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Button, Form } from "semantic-ui-react";
+
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
+
+import { useForm } from "../util/hooks";
+
+const PostForm = () => {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: "",
+  });
+
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(_, result) {
+      console.log(result);
+      values.body = "";
+    },
+    onError(error) {
+      console.log(error.graphQLErrors[0].message);
+      console.log("dfdfd");
+      console.log(error);
+    },
+    refetchQueries: [
+      {
+        query: FETCH_POSTS,
+      },
+    ],
+  });
+
+  function createPostCallback() {
+    createPost();
+  }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a post: </h2>
+      <Form.Field>
+        <Form.Input
+          placeholder="Hi World"
+          name="body"
+          onChange={onChange}
+          error={error && error.graphQLErrors[0].message}
+        />
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  );
+};
+
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+    }
+  }
+`;
+
+const FETCH_POSTS = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
+export default PostForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,54 +1,66 @@
-import React from 'react'
-import {useQuery} from '@apollo/react-hooks';
-import { Grid } from 'semantic-ui-react'
-import gql from 'graphql-tag';
+import React, { useContext } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Grid, Transition } from "semantic-ui-react";
+import gql from "graphql-tag";
 
-import PostCard from '../components/PostCard';
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
 
-const  Home = () =>{
-    //const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS);
-    const {loading, data} = useQuery(FETCH_POSTS);
+import { AuthContext } from "../context/auth";
 
-    return (
-        <Grid columns={3} divided>
-            <Grid.Row className="page-title">
-                <h1>Recent Posts</h1>
-            </Grid.Row>
-            <Grid.Row>
-            {loading ? (
-                <p>loading...</p>
-            ): (
-                data.getPosts  && data.getPosts.map(post =>(
-                <Grid.Column key={post.id} style={{marginBotton: 20}}>
-                    <PostCard post={post}/>
-                </Grid.Column>
-                ))
-            )}
-            </Grid.Row>
-        </Grid>
-    )
-}
- 
+const Home = () => {
+  //const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS);
+  const { loading, data } = useQuery(FETCH_POSTS);
+
+  const { user } = useContext(AuthContext);
+  return (
+    <Grid columns={3} divided>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <Transition.Group>
+            {data.getPosts && data.getPosts.map(post =>(
+            <Grid.Column key={post.id} style={{ marginBotton: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+            ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
+  );
+};
+
 const FETCH_POSTS = gql`
-{
-    getPosts{
-        id
-        body
-        createdAt
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
         username
-        likeCount
-        likes{
-            username
-        }
-        commentCount
-        comments{
-            id
-            username
-            createdAt
-            body
-        }
-        
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
     }
-}
+  }
 `;
 export default Home;
