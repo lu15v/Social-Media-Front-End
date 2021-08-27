588300585f23d0d5b331b7b34794f6fb7edf5602
lu15v <lu15bytes@gmail.com>
2021-08-27 00:45:25 -0500

    CHORE: indentation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import SinglePost from './pages/SinglePost';
-import MenuBar from './components/MenuBar';
-import {Container} from 'semantic-ui-react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SinglePost from "./pages/SinglePost";
+import MenuBar from "./components/MenuBar";
+import { Container } from "semantic-ui-react";
 
-import {AuthProvider} from './context/auth';
-import AuthRoute from './util/authRoute';
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/authRoute";
 
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
-
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
 function App() {
   return (
-   <AuthProvider>
-        <Router>
+    <AuthProvider>
+      <Router>
         <Container>
-            <MenuBar/>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/login' component={Login}/>
-            <Route exact path='/register' component={Register}/>
-            <AuthRoute exact path='/posts/:postId' component={SinglePost}/>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <AuthRoute exact path="/posts/:postId" component={SinglePost} />
         </Container>
       </Router>
-   </AuthProvider>
+    </AuthProvider>
   );
 }
 
